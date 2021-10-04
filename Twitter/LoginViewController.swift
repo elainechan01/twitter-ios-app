664756eb7468003b8dcccc383bac7eafdabe45d7
Elaine <71743181+elainechan01@users.noreply.github.com>
2021-10-04 16:14:00 -0400

    first stage
    - update launch screen
    - add user login page

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Elaine Chan on 10/4/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    let myUrl = "https://api.twitter.com/oauth/request_token"
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("Something went wrong.")
+        })
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
